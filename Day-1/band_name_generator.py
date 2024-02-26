724385097fc567a464e73b3ff2e75f589b6ad648
Piyush Pawar <piyushpawar274@gmail.com>
2024-02-26 19:44:33 +0530

    added spaces

--- Day-1/band_name_generator.py
+++ Day-1/band_name_generator.py
@@ -3,4 +3,4 @@
 print("Welcome to the Band Name Generator!")
 city = input("What city your were brought up in?\n")
 pet = input("What is the name of your pet?\n")
-print("Your Band name is "+city+" "+pet)
+print("Your Band name is "+ city +" "+ pet)
