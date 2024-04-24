53a700d6d7133855582af8c9af8655d24951cf2b
Piyush Pawar <piyushpawar274@gmail.com>
2024-04-24 10:48:35 +0530

    Third day of codebasics python - session 3

--- Python-CodeBasics/itreators.py
+++ Python-CodeBasics/itreators.py
@@ -8,4 +8,24 @@
 # print(next(itr))
 
 class remotecontrol():
+    def __init__(self):
+        self.channels = ["HBO","cnn","bbc","espn"]
+        self.index = -1
+
+    def __iter__(self):
+        return self    
     
+    def __next__(self):
+        self.index += 1
+        if self.index == len(self.channels):
+            raise StopIteration
+        return self.channels[self.index]
+
+r = remotecontrol()        
+itr = iter(r)
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
+
